ff6789e3a5c42a22fad2dca72031f7679b7a1101
Maga Bashirov <magabashirov@Magas-MacBook-Pro-2.local>
2023-06-28 13:29:17 +0300

    redux part 1 lessons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as actions from "./store/actions";
+import { initiateStore } from "./store/store";
+import {titleChanged} from "./store/actions";
+
+
+const store = initiateStore();
+const App = () => {
+
+  const [state, setState] = useState(store.getState());
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+  
+  const completeTask = (taskId) => {
+    store.dispatch(actions.taskCompleted(taskId));
+  };
+
+  const changeTitle = (taskId) => {
+    store.dispatch(titleChanged(taskId));
+  };
+
+  return (
+    <>
+
+      <h1>App</h1>
+      <ul>
+        {state.map(el =>
+          <li key={`task + ${el.id}`}>
+            <p>{el.title}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <hr />
+          </li>)}
+      </ul>
+    </>
+  );
+};
+
+export default App;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +50,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
